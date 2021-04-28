ca7205057098c138a0e8c6f8e31008909970065d
Chris McGraw <chris.rmcgraw1@gmail.com>
2021-04-28 13:18:50 -0400

    create initial geoIpApi service

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -29,37 +29,37 @@ export class MapComponent implements AfterViewInit {
 
 // ---
 
-    setTimeout(() => {
-      L.marker([47.622868, -122.336553], {
-        icon: posIcon
-      }).addTo(map);
-
-      map.setView([47.622868, -122.336553], 2);
-    }, 5000);
-
-    setTimeout(() => {
-      map.setView([47.622868, -122.336553], 4.5);
-    }, 6000);
-
-    setTimeout(() => {
-      map.setView([47.622868, -122.336553], 7);
-    }, 6500);
-
-    setTimeout(() => {
-      map.setView([47.622868, -122.336553], 9.5);
-    }, 7500);
-
-    setTimeout(() => {
-      map.setView([47.622868, -122.336553], 12);
-    }, 8000);
-
-    setTimeout(() => {
-      map.setView([47.622868, -122.336553], 14.5);
-    }, 9000);
-
-    setTimeout(() => {
-      map.setView([47.622868, -122.336553], 17);
-    }, 9500);
+    // setTimeout(() => {
+    //   L.marker([47.622868, -122.336553], {
+    //     icon: posIcon
+    //   }).addTo(map);
+    //
+    //   map.setView([47.622868, -122.336553], 2);
+    // }, 5000);
+    //
+    // setTimeout(() => {
+    //   map.setView([47.622868, -122.336553], 4.5);
+    // }, 6000);
+    //
+    // setTimeout(() => {
+    //   map.setView([47.622868, -122.336553], 7);
+    // }, 6500);
+    //
+    // setTimeout(() => {
+    //   map.setView([47.622868, -122.336553], 9.5);
+    // }, 7500);
+    //
+    // setTimeout(() => {
+    //   map.setView([47.622868, -122.336553], 12);
+    // }, 8000);
+    //
+    // setTimeout(() => {
+    //   map.setView([47.622868, -122.336553], 14.5);
+    // }, 9000);
+    //
+    // setTimeout(() => {
+    //   map.setView([47.622868, -122.336553], 17);
+    // }, 9500);
   }
 
   constructor() { }
